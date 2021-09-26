2e290155367efff2b36a3d03b5138a7b2050e488
davidcoats <david@getsafetycone.com>
2021-09-26 10:11:49 -0700

    Initial commit of source-stub.

---
+++ source/R5T.T0048.Construction/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0048.Construction
+{
+    /// <summary>
+    /// Construction project for General file index type and operator..
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
---
+++ source/R5T.T0048/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0048
+{
+    /// <summary>
+    /// General file index type and operator.
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
